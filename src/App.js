bf6489c746aea4d6fd42fe11118f36d5b260d15b
RelishedChicken <veerajvadgama@gmail.com>
2023-11-03 20:52:48 +0000

    build basic question and collection process

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback } from 'react';
+import $ from "jquery";
+import { Survey } from 'survey-react-ui';
+import { Model } from 'survey-core';
+import { quiz } from './Quiz';
+import { competitors } from './Competitors';
+import { winners } from './Winners';
+import { theme } from "./Theme";
+import './index.css';
+import 'survey-core/defaultV2.min.css';
 
 function App() {
+
+  //Breakdown competitor data
+  const allDrivers = [];
+  const allConstructors = [];
+  const allNonWinners = [];
+  for(const [constructor,drivers] of Object.entries(competitors)){
+    allConstructors.push(constructor);
+    drivers.forEach(driver => {
+      allDrivers.push(driver);
+    })
+  }
+  for(const [type,drivers] of Object.entries(winners)){
+    if(type==='NonRaceWinners'){
+      drivers.forEach(driver => {
+        allNonWinners.push(driver);
+      })
+    }
+  }
+
+  //Intersect the real data in
+  quiz.pages.forEach(page => {
+    page.elements.forEach(question => {
+      if(question.dataNeeded === 'driver'){
+        question.choices = allDrivers;
+      }else if(question.dataNeeded === 'constructor'){
+        question.choices = allConstructors;
+      }else if(question.dataNeeded === 'nonwinners'){
+        question.choices = allNonWinners;
+      }
+    });
+  });
+  
+  //Survey setup
+  const survey = new Model(quiz);
+  survey.applyTheme(theme);
+  const surveyComplete = useCallback((sender) => {
+    saveSurveyResults(sender.data);
+  })
+  survey.onComplete.add(surveyComplete);
+
+  //Render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Formula One 2024 Season Predictor</h1>
+      <Survey model={survey} />
     </div>
   );
+}
+
+function saveSurveyResults(json) {
+  //Save to BigQuery!
+  alert(JSON.stringify(json));
 }
 
 export default App;
